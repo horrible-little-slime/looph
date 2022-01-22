24f10eca06afe9fb220e8de270a0ff249fceb75f
horrible little slime <69secret69email69@gmail.com>
2022-01-21 19:40:24 -0500

    initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,79 @@
-import { myMp, print } from "kolmafia";
+import {
+  abort,
+  canInteract,
+  cliExecute,
+  itemAmount,
+  myLevel,
+  print,
+  retrieveItem,
+  use,
+  visitUrl,
+} from "kolmafia";
+import {
+  $effect,
+  $familiar,
+  $item,
+  $items,
+  $location,
+  $skill,
+  $slot,
+  adventureMacroAuto,
+  Clan,
+  get,
+  have,
+  StrictMacro,
+  uneffect,
+} from "libram";
+import Outfit from "./outfit";
+Clan.withStash("Alliance From Heck", $items`repaid diaper`, () =>
+  Clan.withStash("Hobopolis Vacation Home", $items`meteorite necklace`, () => {
+    cliExecute("phccs_gash softcore");
+    if (myLevel() !== 1) abort("Failed to ascend!");
+    cliExecute("phccs");
 
-export function checkMP(): string {
-  if (myMp() < 200) {
-    return "Your MP is less than 200.";
-  } else {
-    return "Your MP is greater than or equal to 200.";
-  }
-}
+    if (!get("csServicesPerformed").includes("Make Sausage")) abort("Failed to complete CS run!");
+  })
+);
 
-export default function main(): void {
-  print(checkMP());
+if (!canInteract()) {
+  visitUrl("council.php");
+  visitUrl("choice.php?whichchoice=1089&option=30");
 }
+if (!get("kingLiberated")) abort("Failed to donate body!");
+
+cliExecute("pull all");
+if (have($item`can of Rain-Doh`)) use(1, $item`can of Rain-Doh`);
+
+Clan.withStash("Alliance From Heck", $items`Pantsgiving`, (borrowedItems: Item[]) => {
+  if (!borrowedItems.includes($item`Pantsgiving`)) abort("No pants!");
+  retrieveItem(6, $item`heat-resistant sheet metal`);
+  new Outfit(
+    new Map<Slot, Item>([
+      [$slot`hat`, $item`Iunion Crown`],
+      [$slot`back`, $item`Buddy Bjorn`],
+      [$slot`shirt`, $item`BGE 'cuddly critter' shirt`],
+      [$slot`weapon`, $item`garbage sticker`],
+      [$slot`off-hand`, $item`cursed magnifying glass`],
+      [$slot`pants`, $item`Pantsgiving`],
+      [$slot`acc1`, $item`mafia thumb ring`],
+      [$slot`acc2`, $item`lucky gold ring`],
+      [$slot`acc3`, $item`Mr. Cheeng's spectacles`],
+      [$slot`familiar`, $item`orange boxing gloves`],
+    ]),
+    $familiar`Puck Man`
+  ).with(() => {
+    const volcoinos = itemAmount($item`Volcoino`);
+    const volcoinoCheck = () =>
+      itemAmount($item`Volcoino`) > volcoinos + (get("_luckyGoldRingVolcoino") ? 1 : 0);
+    while (!volcoinoCheck() && !have($effect`Beaten Up`)) {
+      adventureMacroAuto(
+        $location`The Bubblin' Caldera`,
+        StrictMacro.skill($skill`Weapon of the Pastalord`).item($item`exploding cigar`)
+      );
+    }
+    cliExecute("hottub");
+  });
+  uneffect($effect`Cowrruption`);
+});
+cliExecute("breakfast");
+print("You're ready for garbo, cowboy!");
---
+++ src/outfit.ts
@@ -0,0 +1,114 @@
+import { canEquip, equip, equippedAmount, equippedItem, myFamiliar, useFamiliar } from "kolmafia";
+import { $item, $slot, $slots, have } from "libram";
+
+export default class Outfit {
+  equips: Map<Slot, Item>;
+  familiar?: Familiar;
+
+  /**
+   * Construct an outfit object, for rapid equipping
+   * @param equips Map of what to equip and where
+   * @param familiar Optional familiar to use with outfit
+   */
+  constructor(equips: Map<Slot, Item>, familiar?: Familiar) {
+    this.equips = equips;
+    this.familiar = familiar;
+  }
+
+  dress(): void {
+    if (this.familiar) useFamiliar(this.familiar);
+    const targetEquipment = Array.from(this.equips.values());
+    const accessorySlots = $slots`acc1, acc2, acc3`;
+    for (const slot of $slots`weapon, off-hand, hat, shirt, pants, familiar, buddy-bjorn, crown-of-thrones, back`) {
+      if (
+        targetEquipment.includes(equippedItem(slot)) &&
+        this.equips.get(slot) !== equippedItem(slot)
+      )
+        equip(slot, $item`none`);
+    }
+
+    //Order is anchored here to prevent DFSS shenanigans
+    for (const slot of $slots`weapon, off-hand, hat, back, shirt, pants, familiar, buddy-bjorn, crown-of-thrones`) {
+      const equipment = this.equips.get(slot);
+      if (equipment) equip(slot, equipment);
+    }
+
+    //We don't care what order accessories are equipped in, just that they're equipped
+    const accessoryEquips = accessorySlots
+      .map((slot) => this.equips.get(slot))
+      .filter((item) => item !== undefined) as Item[];
+    for (const slot of accessorySlots) {
+      const toEquip = accessoryEquips.find(
+        (equip) =>
+          equippedAmount(equip) < accessoryEquips.filter((accessory) => accessory === equip).length
+      );
+      if (!toEquip) break;
+      const currentEquip = equippedItem(slot);
+      //We never want an empty accessory slot
+      if (
+        currentEquip === $item`none` ||
+        equippedAmount(currentEquip) >
+          accessoryEquips.filter((accessory) => accessory === currentEquip).length
+      ) {
+        equip(slot, toEquip);
+      }
+    }
+  }
+
+  /**
+   * Identical to withOutfit; executes callback function with equipped outfit, and returns to original outfit
+   * @param callback Function to execute
+   */
+  with<T>(callback: () => T): T {
+    return withOutfit(this, callback);
+  }
+
+  /**
+   * Makes the best outfit it can with what you've got
+   * @param equips Map of what to equip and where; will use first item in array that it can, and willl not add things to outfit otherwise
+   * @param familiar Optional familiar to use with outfit
+   */
+  static doYourBest(equips: Map<Slot, Item | Item[]>, familiar?: Familiar): Outfit {
+    const returnValue = new Map<Slot, Item>();
+    for (const [slot, itemOrItems] of equips.entries()) {
+      const item = Array.isArray(itemOrItems)
+        ? itemOrItems.find((item) => have(item) && (slot === $slot`familiar` || canEquip(item)))
+        : itemOrItems;
+      if (item) returnValue.set(slot, item);
+    }
+    return new Outfit(returnValue, familiar);
+  }
+
+  /**
+   * Saves current outfit as an Outfit
+   * @param withFamiliar Option to store current familiar as part of outfit
+   */
+  static current(withFamiliar = false): Outfit {
+    const familiar = withFamiliar ? myFamiliar() : undefined;
+    const slots = $slots`hat, shirt, back, weapon, off-hand, pants, acc1, acc2, acc3`;
+    if (withFamiliar) slots.push($slot`familiar`);
+    const outfitMap = new Map<Slot, Item>();
+    for (const slot of slots) {
+      const item = equippedItem(slot);
+      if (item !== $item`none`) outfitMap.set(slot, item);
+    }
+    return new Outfit(outfitMap, familiar);
+  }
+}
+
+/**
+ * Execute callback while wearing given outfit
+ * Then return to what you were previously wearing
+ * @param outfit Outfit to use
+ * @param callback Function to execute
+ */
+export function withOutfit<T>(outfit: Outfit, callback: () => T): T {
+  const withFamiliar = outfit.familiar !== undefined;
+  const cachedOutfit = Outfit.current(withFamiliar);
+  outfit.dress();
+  try {
+    return callback();
+  } finally {
+    cachedOutfit.dress();
+  }
+}
